b7f84892702bc5493297e14e98c4d9645f2e9a78
Shubham Tyagi <shubhamtyagi8871@gmail.com>
2023-12-17 13:52:18 +0530

    yes file is uploaded

---
+++ code/yes.java
@@ -0,0 +1,8 @@
+package code;
+
+public class yes {
+    public static void main(String[] args) {
+        System.out.println("Checking github");
+        System.out.println("yes its good");
+    }
+}
