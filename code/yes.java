69c1f2ca23a54061416f32900b3a0770a9b8339f
Shubham Tyagi <shubhamtyagi8871@gmail.com>
2023-12-17 14:12:39 +0530

    Instructions added

--- code/yes.java
+++ code/yes.java
@@ -2,7 +2,7 @@ package code;
 
 public class yes {
     public static void main(String[] args) {
-        System.out.println("Checking github");
+        System.out.println("Checking github"); // telling the sporce code
         System.out.println("yes its good");
     }
 }
